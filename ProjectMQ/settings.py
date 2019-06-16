fdb02d8b58425b102915804478cc5d256f272a3d
Akshat <akshat.khanna@st.niituniversity.in>
2019-06-16 22:56:20 +0530

    server IP

--- ProjectMQ/settings.py
+++ ProjectMQ/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '6z=!y%_=ig08k_^i%ehlti^q&q30kbxjawv0+v=ea^o9f&1afm'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1","localhost","0.0.0.0",".ngrok.io","3.83.129.144"]
 
 
 # Application definition
@@ -86,12 +86,12 @@ CHANNEL_LAYERS = {
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 # Password validation
